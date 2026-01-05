c90f7c237adac617b678c969c52b34ae3f72c173
FeestFundament <info@feest-fundament.nl>
2026-01-05 21:09:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App = () => (
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </Hashrouter>
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
